cb9f14f53fe152235e4cee1759cff68bdd0fa69d
titus-mumo <tituskennedy74@gmail.com>
2023-05-10 15:52:01 +0300

    Adding additional features

--- AI/models.py
+++ AI/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
+class Patient(models.Model):
+    patient_id = models.PositiveIntegerField(primary_key=True, unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    patient_email = models.CharField(max_length=30)
+    mobile = models.IntegerField()
+    password = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
